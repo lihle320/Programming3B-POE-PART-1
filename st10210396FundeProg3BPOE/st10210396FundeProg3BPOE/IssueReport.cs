dfbc2244c52fffd972945aeb294d522f7ab872b0
lihle320 <126753936+lihle320@users.noreply.github.com>
2025-09-09 15:01:57 +0200

    Update

--- st10210396FundeProg3BPOE/st10210396FundeProg3BPOE/IssueReport.cs
+++ st10210396FundeProg3BPOE/st10210396FundeProg3BPOE/IssueReport.cs
@@ -23,7 +23,7 @@ namespace st10210396FundeProg3BPOE
         }
     }
 }
-//Referrnces 
+//References 
 //Microsoft Learn. (n.d.).OpenFileDialog Class(System.Windows.Forms). [Online].Available at: https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.openfiledialog?view=windowsdesktop-9.0 [Accessed 03 September2025].
 //Microsoft Learn. 2025.How to open a message box, 05 July 2025. Online]. Available at: https://learn.microsoft.com/en-us/dotnet/desktop/wpf/windows/how-to-open-message-box [Accessed 03 September2025].
 //Open Form2 From Form1 And Hide Form1 And Show Form1 Again After Closing The Form2 | C# Tutorial. 2021. YouTube video. Added by #SmartCode. [Online]. Available at: https://www.youtube.com/watch?v=bb5TaLbq9aM [Accessed 03 September2025].
--- st10210396FundeProg3BPOE/st10210396FundeProg3BPOE/ReportIssues.cs
+++ st10210396FundeProg3BPOE/st10210396FundeProg3BPOE/ReportIssues.cs
@@ -276,3 +276,9 @@ namespace st10210396FundeProg3BPOE
     }
 
 }
+//References 
+//Microsoft Learn. (n.d.).OpenFileDialog Class(System.Windows.Forms). [Online].Available at: https://learn.microsoft.com/en-us/dotnet/api/system.windows.forms.openfiledialog?view=windowsdesktop-9.0 [Accessed 03 September2025].
+//Microsoft Learn. 2025.How to open a message box, 05 July 2025. Online]. Available at: https://learn.microsoft.com/en-us/dotnet/desktop/wpf/windows/how-to-open-message-box [Accessed 03 September2025].
+//Open Form2 From Form1 And Hide Form1 And Show Form1 Again After Closing The Form2 | C# Tutorial. 2021. YouTube video. Added by #SmartCode. [Online]. Available at: https://www.youtube.com/watch?v=bb5TaLbq9aM [Accessed 03 September2025].
+//Microsoft Learn. (n.d.). LinkedList<T> Class. [Online]. Available at: https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.linkedlist-1?view=net-8.0 [Accessed 03 September2025]. 
+
